56a0891978cc7b36493ddd314b839f9905d1c0dd
asmaakram77 <asmaakram77@yahoo.com>
2015-12-10 19:13:24 +0500

    hello

---
+++ app/models/product.rb
@@ -0,0 +1,4 @@
+class Product < ActiveRecord::Base
+  attr_accessible :age, :description, :name
+  has_many :comments
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ECommerce::Application.config.secret_token = '18caec1206a163463173ceacf79650d19128a0b2309cb3aab16eb032f7ae0fca59202fd7e7b7187aa80800042c256a4735dc384360aea94185a60138cfde454d'
